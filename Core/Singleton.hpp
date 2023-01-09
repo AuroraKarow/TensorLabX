010024ed7c193b41031962c7f11f8f0120de25d3
cAtAphrACtCtl <1472419561@qq.com>
2023-01-09 13:22:23 +0800

    Singleton

--- Core/Singleton.hpp
+++ Core/Singleton.hpp
@@ -10,16 +10,16 @@ protected:
     static std::shared_ptr<T> instance;
 
 public:
-    static T *Instance();
+    static std::shared_ptr<T>& Instance();
 };
 
 template <class T>
 std::shared_ptr<T> Singleton<T>::instance{new T()};
 
 template <class T>
-T *Singleton<T>::Instance()
+std::shared_ptr<T>& Singleton<T>::Instance()
 {
-    return instance.get();
+    return instance;
 }
 
 #define MAKE_SINGLETON(Type)      \
