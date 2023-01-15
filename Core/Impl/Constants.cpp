2b830476a24296909655966cd0e2075ea00b20cb
Cataphract <44166626+cAtAphrACtCtl@users.noreply.github.com>
2023-01-15 08:21:33 +0800

    clean files

--- Core/Impl/Constants.cpp
+++
@@ -1,9 +0,0 @@
-#include "../Constants.hpp"
-#include <cmath>
-using namespace std;
-namespace Core
-{
-	// RConstants::RConstants() :
-	// 	Pi(acosl(-1)) {
-	// }
-}
--- Core/Impl/Helper.cpp
+++
@@ -1,9 +0,0 @@
-#include "../Helper.hpp"
-namespace Core
-{
-	namespace Helper
-	{
-		// bool MatrixHelper::ElementWiseEqual(void *a, ui32 sizeofA, void *b, ui32 sizeofB)
-		
-	}
-}
